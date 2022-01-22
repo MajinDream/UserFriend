b879cb34385b7ba7cfa3259ef0c64ce85c235e57
Dias Manap <diasmanap1@gmail.com>
2022-01-22 16:40:51 +0600

    Initial Commit

---
+++ UserFriend/UserFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserFriendApp.swift
+//  UserFriend
+//
+//  Created by Dias Manap on 22.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct UserFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
